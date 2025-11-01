d62a9f3b34ec84fb5e1181a4bb19926366e5552b
Timan Zheng <timanz100@gmail.com>
2025-11-01 15:05:29 -0400

    first commit

---
+++ springqpro/src/main/java/com/springqprobackend/springqpro/SpringqproApplication.java
@@ -0,0 +1,13 @@
+package com.springqprobackend.springqpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringqproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringqproApplication.class, args);
+	}
+
+}
---
+++ springqpro/src/test/java/com/springqprobackend/springqpro/SpringqproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springqprobackend.springqpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringqproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
