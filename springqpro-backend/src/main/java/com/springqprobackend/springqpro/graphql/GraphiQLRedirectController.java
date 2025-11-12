e794ba5add914c0ac43ea45dade0892928b4949d
Timan Zheng <timanz100@gmail.com>
2025-11-11 19:25:01 -0500

    Fixed the GraphiQL issue I had. Spring Boot no longer serves GraphiQL automatically at /graphiql because GraphiiQL 5.x+ has moved to an ESM-only build, breaking the old webjars-based static resource inclusion that Spring Boot used. So to include GraphiQL -- pretty much purely for testing purposes (not super integral or anything) -- just need to create it from a static HTML file that loads GraphiQL from a CDN, which I took from here: https://github.com/graphql/graphiql/issues/4033#issuecomment-3001804909 Also added a re-direct controller mapper. Facing a weird issue where sometimes application fails to start, but can't really determine the cause tbh (sporadic occurrences post-rebooting, suspect it has to do with dependencies).

---
+++ springqpro-backend/src/main/java/com/springqprobackend/springqpro/graphql/GraphiQLRedirectController.java
@@ -0,0 +1,12 @@
+package com.springqprobackend.springqpro.graphql;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GraphiQLRedirectController {
+    @GetMapping({"/graphiql", "/graphiql/"})
+    public String redirect() {
+        return "redirect:/graphiql/index.html";
+    }
+}
