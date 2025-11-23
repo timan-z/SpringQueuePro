0a15fb8691dca469a3e496065881ab9afcd58e43
Timan Zheng <timanz100@gmail.com>
2025-11-23 13:05:09 -0500

    Added Rediscontainer dependency and refactored my RedisPingIntegrationTest to just use that instead (don't need the dynamic prop stuff) -- better and more efficient/proper.

--- springqpro-backend/src/test/java/com/springqprobackend/springqpro/integration/RedisPingIntegrationTest.java
+++ springqpro-backend/src/test/java/com/springqprobackend/springqpro/integration/RedisPingIntegrationTest.java
@@ -1,10 +1,12 @@
 package com.springqprobackend.springqpro.integration;
 
+import com.redis.testcontainers.RedisContainer;
 import com.springqprobackend.springqpro.config.RedisTestConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -14,6 +16,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 //@ContextConfiguration(classes = { RedisPingIntegrationTest.Config.class })
 //@Import(RedisTestConfig.class)
@@ -21,8 +24,22 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 @ExtendWith(SpringExtension.class)
 class RedisPingIntegrationTest {
-    // Testcontainers Redis:
     @Container
+    @ServiceConnection
+    static final RedisContainer REDIS =
+            new RedisContainer(DockerImageName.parse("redis:7.2"));
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    void testPing() {
+        String result = stringRedisTemplate.execute((RedisCallback<String>) (conn) -> conn.ping());
+        assert result.equalsIgnoreCase("PONG");
+    }
+    // OLD:
+    // Testcontainers Redis:
+    /*@Container
     static final GenericContainer<?> REDIS =
             new GenericContainer<>("redis:7.2")
                     .withExposedPorts(6379);
@@ -40,5 +57,5 @@ class RedisPingIntegrationTest {
     void testPing() {
         String result = stringRedisTemplate.execute((RedisCallback<String>) (conn) -> conn.ping());
         assert result.equalsIgnoreCase("PONG");
-    }
+    }*/
 }
