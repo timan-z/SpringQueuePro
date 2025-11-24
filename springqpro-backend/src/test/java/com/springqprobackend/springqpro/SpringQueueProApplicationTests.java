ebc6a0acd069dd374be822fafda52370c8ca2179
Timan Zheng <timanz100@gmail.com>
2025-11-23 22:05:24 -0500

    DEBUG-COMMIT: Trying to CI to work w/ Redis [5]

--- springqpro-backend/src/test/java/com/springqprobackend/springqpro/SpringQueueProApplicationTests.java
+++ springqpro-backend/src/test/java/com/springqprobackend/springqpro/SpringQueueProApplicationTests.java
@@ -1,9 +1,14 @@
 package com.springqprobackend.springqpro;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ImportAutoConfiguration(exclude = {
+        com.springqprobackend.springqpro.config.RedisConfig.class,
+        com.springqprobackend.springqpro.config.RedisTestConfig.class
+})
 class SpringQueueProApplicationTests {
 
 	@Test
